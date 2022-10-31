46aaf2f4091e75aa1f7f14e327f3e9a54c3af402
The Changer <thechanger412@gmail.com>
2022-10-31 13:41:13 -0500

    inital commit

---
+++ src/main/java/com/the_changer/pwtd/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package com.the_changer.pwtd.mixin;
+
+import com.the_changer.pwtd.pwtdMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		pwtdMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
